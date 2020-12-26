06a7235ecdd63b1b199f2ed8de57922ae4977295
Rokibul Islam <rokib.hridoy@gmail.com>
2020-12-27 00:57:12 +0600

    -_-

--- LA/settings.py
+++ LA/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'x8*(b8d9463re+t)2mgi+vrbh7!9(s6_^6c5j(76yoqy!hp(19'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.1.7', 'localhost', '192.168.1.9']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.7', 'localhost', 'little-angels.herokuapp.com']
 
 
 # Application definition
