f17497ba990233d417f2f21f62f4071cf92a0eea
Rokibul Islam <rokib.hridoy@gmail.com>
2020-12-27 01:49:30 +0600

    fkkk

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 
 def home(request):
     return render(request, "home/home.html", {'title': 'home'})
 
 
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
